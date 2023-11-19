47cbe2aa3b70ec9db86a3bb8cdc2f8e05b9d2cd8
bhas <bhaskar@theretailinsights.com>
2023-11-19 22:01:05 +0530

    serving react app in from express

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import "dotenv/config";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 //all middlewares
+app.use(express.static(join(__dirname, "public", "client/build")));
 
 //all routes
-
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public", "client/build"));
+});
 
 //dbconnections
 const PORT = process.env.PORT || 3000;
